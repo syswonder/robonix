c0ffd137300e3a08d508baeb927a7f6c46b6bf84
lhw2002426 <75192950+lhw2002426@users.noreply.github.com>
2025-07-13 19:10:39 +0800

    use plugin to impl cap

---
+++ capability/navigation2/api/__init__.py
@@ -0,0 +1,7 @@
+# AUTO-GENERATED FILE - DO NOT EDIT MANUALLY
+
+# Imports for decorated API functions
+from navigation2.plugin.ros2_navigation.lib import nv_test
+
+# Import the original API module
+from .api import *
