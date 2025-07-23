2f27a71e125d6f43d1fb82259e576e80b6cdf66c
wheatfox <wheatfox17@icloud.com>
2025-07-23 22:00:06 +0800

    Remove deprecated Genesis symlink and add new gRPC-based robot control implementation with movement and rotation functionalities, along with initial setup for virtual entity graph management.

--- manager/graph/__init__.py
+++ manager/graph/__init__.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 __version__ = "0.0.1"
-__author__ = "wheatfox"
+__author__ = "wheatfox <wheatfox17@icloud.com>"
 __description__ = "Virtual Entity Graph for EOS Programming"
