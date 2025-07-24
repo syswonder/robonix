d7588fdf03297167485d68f2bd8447ec81fd06f4
wheatfox <wheatfox17@icloud.com>
2025-07-24 10:06:54 +0800

    Refactor primitive specifications in primitive_spec.py to utilize EAIOS_XYZ and EAIOS_IMAGE for improved clarity and consistency in argument and return types.

--- manager/specs/primitive_spec.py
+++ manager/specs/primitive_spec.py
@@ -14,24 +14,29 @@ class EAIOS_IMAGE_FORMAT(Enum):
     WEBP = "webp"
 
 
+EAIOS_XYZ = {"x": float, "y": float, "z": float}
+
+# TODO: add recursive type check in entity
+EAIOS_IMAGE = {
+    "image_raw": bytes,
+    "metadata": {
+        "width": int,
+        "height": int,
+        "format": EAIOS_IMAGE_FORMAT,
+    },
+}
+
 PRIMITIVE_SPECS = {
     "getpos": {
         "args": EAIOS_PRIMITIVE_NO_ARGS,
-        "returns": {"x": float, "y": float, "z": float},
+        "returns": EAIOS_XYZ,
     },
     "move": {
-        "args": {"x": float, "y": float, "z": float},
+        "args": EAIOS_XYZ,
         "returns": {"success": bool},
     },
     "capture": {
         "args": EAIOS_PRIMITIVE_NO_ARGS,
-        "returns": {
-            "image_raw": bytes,
-            "metadata": {
-                "width": int,
-                "height": int,
-                "format": EAIOS_IMAGE_FORMAT,
-            },
-        },
+        "returns": EAIOS_IMAGE,
     },
 }
