59f6fc215fdab443f045dfc8638c7f3807f2fea8
wheatfox <wheatfox17@icloud.com>
2025-07-24 09:53:17 +0800

    Enhance logging in entity.py to include absolute paths for better traceability during primitive execution. Refactor flow1.py to improve readability by storing positions in variables before moving entities. Update resolution in robot1.py for improved display quality.

--- simulator/flow1/flow1.py
+++ simulator/flow1/flow1.py
@@ -55,14 +55,16 @@ def sim_run_flow():
     book1 = root.get_entity_by_path("room/book1")
     ranger = root.get_entity_by_path("room/ranger")
     plant_pot = root.get_entity_by_path("room/plant_pot")
-    print(f"book1 at entity_path {book1.get_absolute_path()}, ranger at entity_path {ranger.get_absolute_path()}")
+    print(
+        f"book1 at entity_path {book1.get_absolute_path()}, ranger at entity_path {ranger.get_absolute_path()}"
+    )
 
     book1_pos = book1.getpos()
     print(f"book1_pos = {book1_pos}")
-    
-    # ranger.move(x=book1_pos["x"], y=book1_pos["y"] + 0.5, z=book1_pos["z"])
+
+    ranger.move(x=book1_pos["x"], y=book1_pos["y"] + 0.5, z=book1_pos["z"])
     # ranger.move(x=0, y=0, z=0)
-    ranger.move(x="hello", y=0.2, z={"hello1": "world2"})
+    # ranger.move(x="hello", y=0.2, z={"hello1": "world2"})
 
     print(f"finished moving to book1, ranger now at {ranger.getpos()}")
 
@@ -71,18 +73,21 @@ def sim_run_flow():
     root.get_entity_by_path("room").add_child(virtual_waypoint1)
     print(f"virtual_waypoint1 = {virtual_waypoint1.get_absolute_path()}")
 
+    virtual_waypoint1_pos = virtual_waypoint1.getpos()
+
     ranger.move(
-        x=virtual_waypoint1.getpos()["x"],
-        y=virtual_waypoint1.getpos()["y"],
-        z=virtual_waypoint1.getpos()["z"],
+        x=virtual_waypoint1_pos["x"],
+        y=virtual_waypoint1_pos["y"],
+        z=virtual_waypoint1_pos["z"],
     )
 
     print(f"finished moving to waypoint, ranger now at {ranger.getpos()}")
 
+    plant_pot_pos = plant_pot.getpos()
     ranger.move(
-        x=plant_pot.getpos()["x"] + 0.5,
-        y=plant_pot.getpos()["y"],
-        z=plant_pot.getpos()["z"],
+        x=plant_pot_pos["x"] + 0.5,
+        y=plant_pot_pos["y"],
+        z=plant_pot_pos["z"],
     )
 
     print(f"finished moving to plant_pot, ranger now at {ranger.getpos()}")
