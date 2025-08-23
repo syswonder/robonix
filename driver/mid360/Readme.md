cb24124e8fbc57ea1a1710a4afae7b2ef1b5bed9
ZhengWu <ken4647@outlook.com>
2025-08-23 23:19:44 +0800

    driver: add support for lidar mid360

---
+++ driver/mid360/Readme.md
@@ -0,0 +1,5 @@
+# Livox ROS2 Driver
+
+Livox SDK2 needs to be installed first.
+
+https://github.com/Livox-SDK/Livox-SDK2/blob/master/README.md
--- driver/static_tf/run.sh
+++ driver/static_tf/run.sh
@@ -1,3 +1,4 @@
 source /opt/ros/humble/setup.bash
 
-ros2 run tf2_ros static_transform_publisher --x 0.24 --y 0.00 --z 0 --qx 0 --qy 0 --qz 0.19509 --qw 0.980785   --frame-id base_link --child-frame-id unilidar_lidar
+# ros2 run tf2_ros static_transform_publisher --x 0.24 --y 0.00 --z 0 --qx 0 --qy 0 --qz 0.19509 --qw 0.980785   --frame-id base_link --child-frame-id unilidar_lidar
+ros2 run tf2_ros static_transform_publisher --x 0.24 --y 0.00 --z 0 --qx 0 --qy 0 --qz 0 --qw 1   --frame-id base_link --child-frame-id livox_frame
