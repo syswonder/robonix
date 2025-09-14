d302e6a4cbf4bb053ddf355f97b9e25306bead5d
wheatfox <wheatfox17@icloud.com>
2025-09-14 18:08:35 +0800

    refactor(eaios_decorators): improve code consistency and readability

---
+++ skill/pointcloud/api/__init__.py
@@ -0,0 +1,6 @@
+from .caller import __rpc_skl_spatiallm_detect
+
+# public apis for uapi
+__all__ = [
+    "__rpc_skl_spatiallm_detect"  # when this machine wants to call a skill from an entity that the skill is provided by another machine(skill provider)
+]
