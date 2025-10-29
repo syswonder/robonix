fe928061e30d8580696bc379794eb8a87d511889
wheatfox <wheatfox17@icloud.com>
2025-10-29 08:14:50 +0000

    feat: implement Robonix core with registration service
    - Added core functionality for managing capabilities and skills in Robonix.
    - Implemented registration service with request and response handling.
    - Introduced message structures for registration requests and responses.
    - Created a Docker entrypoint script to source ROS2 environment.
    - Added .gitignore and Cargo configuration for Rust project setup.
    - Included test script for registering a Camera capability via ROS2 service call.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -87,6 +87,9 @@ fi
 # some post apt install
 apt install -qq -y iputils-ping screenfetch >/dev/null 2>&1
 
+# source the setup.bash
+source /opt/ros/humble/setup.bash
+
 echo -e "[*] \033[1mWelcome to robonix docker environment!\033[0m Distro is: \033[33m$(lsb_release -ds 2>/dev/null || echo "Linux")\033[0m with ROS2 \033[33m$(echo $ROS_DISTRO)\033[0m"
 exec bash
 
