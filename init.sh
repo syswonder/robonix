eb3b1233c4f3e50363702ac83d86662f2333170c
wheatfox <wheatfox17@icloud.com>
2026-01-27 16:33:06 +0800

    refactor: simplify CAN interface configuration in init.sh
    Signed-off-by: wheatfox <wheatfox17@icloud.com>

--- init.sh
+++ init.sh
@@ -3,9 +3,6 @@
 sudo ip addr add 192.168.1.50/24 dev enx00e04c360241
 
 # config can
-sudo ip link set can0 up type can bitrate 500000
-sudo ip link set can1 up type can bitrate 500000
-sudo ip link set can2 up type can bitrate 500000
 sudo ip link set can3 up type can bitrate 500000
 
 # config eth connection to raspi4b
